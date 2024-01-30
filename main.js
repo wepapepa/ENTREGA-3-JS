2cccc91cbc6d4d596c1ff6ec2e884c03b59e9620
Pepa Labrin <catu.leibrin@gmail.com>
2024-01-29 21:44:25 -0300

    finished fixing the bug

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-//faltantes: operadores logicos para resumir funciones
-
 document.addEventListener("DOMContentLoaded", () => {
     loadTasks();
 });
 
-function addTask() { //working, i need to add an if else??
+function addTask() {
     const taskInput = document.getElementById("newTask");
-    taskInput.value.trim() === "" ? tasks.push === false : tasks.push === true ;
-
     const tasks = getTasks();
-    tasks.push({ content: taskInput.value, done: false });
-    //tasks.push({ content: checkbox + taskInput.value, done: false }); for the checkbox appearance
 
+    if (taskInput.value.trim() === "") {
+        return; // I abandon the function if its empty
+    }
+
+    tasks.push({ content: taskInput.value, done: false });// if it isnt empty it is added to the list
     saveTasks(tasks);
     renderTasks(tasks);
-    taskInput.value = "";
+
+    taskInput.value = ""; //clean slate
 }
 
 function getTasks() {
     const storedTasks = sessionStorage.getItem("tasks");
-    return storedTasks ? JSON.parse(storedTasks) : [];//working
+    return storedTasks ? JSON.parse(storedTasks) : [];
 }
 
 function saveTasks(tasks) {
@@ -29,34 +29,25 @@ function saveTasks(tasks) {
 function renderTasks(tasks) {
     const listElement = document.getElementById("taskList");
     listElement.innerHTML = '';
-    tasks.forEach((task, index) => {//working but i dont know why it is working
+    tasks.forEach((task, index) => {
         const taskElement = document.createElement("li");
         taskElement.textContent = task.content;
         taskElement.onclick = () => toggleTaskDone(index);
-
-        task.done ? taskElement.style.textDecoration = "line-through" : taskElement.style.textDecoration = "none" ;
+        if (task.done) {
+            taskElement.style.textDecoration = "line-through";
+        }
         listElement.appendChild(taskElement);
     });
 }
 
-function toggleTaskDone(index) { //working
+function toggleTaskDone(index) {
     const tasks = getTasks();
-    tasks[index].done = !tasks[index].done; //operador not 
+    tasks[index].done = !tasks[index].done;
     saveTasks(tasks);
     renderTasks(tasks);
 }
 
-function loadTasks() { //working
+function loadTasks() {
     const tasks = getTasks();
     renderTasks(tasks);
 }
-
-// let checkbox = document.getElementById("checkbox");
-
-// checkbox.addEventListener("click", () => { //fix this i dont know why it isnt working :(
-//     Swal.fire({
-//     title: "Tarea completada!",
-//     text: "Felicidades rockstar!",
-//     icon: "success"
-// });
-// });
